b609014328db60c03f7b04b2748279ea38e95a0a
csx <120781991@qq.com>
2022-08-26 11:03:58 +0800

    delete erigon

--- erigon/config/erigon.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-erigon --datadir /work/data \
-	--chain=goerli --authrpc.addr='0.0.0.0' --authrpc.port='8551' --authrpc.jwtsecret='c9844e44f37b9d63614177a9046d5691bd780ac54edde5095fa0a48e79f44c5c' \
-	--private.api.addr="0.0.0.0:9090" --pprof --pprof.addr="0.0.0.0" --pprof.port="6061"
--- erigon/config/rpcdaemon.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-rpcdaemon \
-        --private.api.addr="erigon:9090" \
-        --txpool.api.addr="erigon:9090" \
-        --ws --http.addr="0.0.0.0" --http.vhosts="*" \
-        --http.corsdomain="*" --http.api="eth,debug,net,web3,trace,rpc,txpool,engine"
